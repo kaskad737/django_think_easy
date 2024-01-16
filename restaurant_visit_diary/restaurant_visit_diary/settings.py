6a0201acc6fa349f851f954111eba653156ab05d
Apti <Archakov.Apti@email.cz>
2024-01-16 18:55:58 +0000

    add all secrets to env

--- restaurant_visit_diary/restaurant_visit_diary/settings.py
+++ restaurant_visit_diary/restaurant_visit_diary/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2#=2uska*^8#jt0@#%0u0tk3#v@y9@-'\
-            '8wio@7-una4=+irswhh'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,11 +85,11 @@ WSGI_APPLICATION = 'restaurant_visit_diary.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_restaurants',
-        'USER': 'postgres',
-        'PASSWORD': 'django-restaurants',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
     }
 }
 
